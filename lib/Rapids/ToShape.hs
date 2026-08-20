9049a3f9f47b926c2ada6b9c1c2b79323740cf11
Adam Vogt <vogt.adam@gmail.com>
2026-08-20 14:59:26 -0400

    instance ToShape [a]

--- lib/Rapids/ToShape.hs
+++ lib/Rapids/ToShape.hs
@@ -6,6 +6,8 @@ import Data.List
 
 class ToShape a where toShape :: a -> Shape
 
+instance ToShape a => ToShape [a] where toShape = foldMap toShape
+
 instance (Double ~ d) => ToShape [V2 d] where toShape abspts = makeShape $ mconcat [line a b :: Path2D | a : b : _ <- tails abspts]
 
 instance ToShape Shape where toShape = id
