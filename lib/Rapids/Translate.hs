00a25d09da100328dbf90f08d3ca883f62a28951
Adam Vogt <vogt.adam@gmail.com>
2026-08-23 08:04:09 -0400

    clean Translate.hs

--- lib/Rapids/Translate.hs
+++ lib/Rapids/Translate.hs
@@ -5,46 +5,69 @@ import Rapids.Color
 import Waterfall (Transformable2D)
 import qualified Waterfall as W
 
--- | Translate a 'Transformable' ( 'Path'/'Solid'/'V3' Double) in one or more directions.
-class Translate r where
-  -- | @translate@ expressions of type 'Transformable' @a => a -> a@ (probably 'Solid' -> 'Solid')
-  --
-  -- > translate x y z
-  -- > translate (v :: V3 Double)
-  -- > translate ex x
-  -- > translate ey y
-  -- > translate ez z
-  translate :: r
+-- * interface
 
-instance {-# INCOHERENT #-} (d ~ Double, e ~ Double, f ~ Double, TranslateGo r t) => Translate (d -> e -> f -> r) where
-  translate x y z = translateGo (W.translate (V3 x y z))
+-- |
+-- > translate
+-- >    x y z
+-- >    (V3 x y z)
+-- >    ex x
+-- >    ey y
+-- >    ez z
+-- >  :: Transformable t => t -> t
+--
+-- t is Solid, V3 Double, Path
+translate :: (TranslateGo r t) => r
+translate = translateGo False (id :: t -> t)
 
-instance {-# INCOHERENT #-} (d ~ Double, PropagateColor a, a ~ a') => Translate (V3 d -> a -> a') where
-  translate v a = propagateColor (W.translate v) a
+-- | 'translate' except it also returns the original
+--
+-- doesn't make much sense for V3 Double
+translated :: (TranslateGo r t) => r
+translated = translateGo True (id :: t -> t)
+
+-- | '_translated' is 'translate' returning an 'Iso''
+_translated :: TranslatedGo r t => r
+_translated = translatedGo id id
+
+-- |
+-- > translate2D
+-- >    x y
+-- >    (V2 x y)
+-- >    ex x
+-- >    ey y
+-- >  :: Transformable2D t => t -> t
+--
+-- t is Shape, V2 Double, Path2D
+translate2D :: (Translate2DGo r t) => r
+translate2D = translate2DGo False (id :: t -> t)
+
+translated2D :: (Translate2DGo r t) => r
+translated2D = translate2DGo True (id :: t -> t)
+
+-- | '_translated2D' is 'translate2D' returning an 'Iso''
+_translated2D :: TranslatedGo r t => r
+_translated2D = translatedGo id id
+
+-- * implementation
 
 class W.Transformable t => TranslateGo r t | r -> t where
-  translateGo :: (t -> t) -> r
+  translateGo :: Bool -> (t -> t) -> r
 
 -- base case
-instance {-# OVERLAPPABLE #-} (PropagateColor a, a' ~ a, a ~ t) => TranslateGo (a -> a') t where
-  translateGo acc = propagateColor acc
+instance {-# OVERLAPPABLE #-} (Num t, PropagateColor a, a' ~ a, a ~ t) => TranslateGo (a -> a') t where
+  translateGo False acc x = propagateColor acc x
+  translateGo True acc x = x + propagateColor acc x
+  -- should intermediates be kept too?
+
 instance {-# INCOHERENT #-} (d ~ Double, e ~ Double, f ~ Double, TranslateGo r t) => TranslateGo (d -> e -> f -> r) t where
-  translateGo acc x y z = translateGo (acc . W.translate (V3 x y z))
+  translateGo keep acc x y z = translateGo keep (acc . W.translate (V3 x y z))
 
 instance {-# OVERLAPPABLE #-} (v ~ V3, amt ~ Double, TranslateGo r t) => TranslateGo (E v -> amt -> r) t where
-  translateGo acc (E e) amt = translateGo (acc . W.translate (0 & e .~ amt))
+  translateGo keep acc (E e) amt = translateGo keep (acc . W.translate (0 & e .~ amt))
 
-instance {-# OVERLAPPABLE #-} (v ~ V3, amt ~ Double, TranslateGo r t) => Translate (E v -> amt -> r) where
-  translate (E e) amt = translateGo (W.translate (0 & e .~ amt))
-
-
-
--- | Translate a 'Transformable' in one or more directions through an 'Iso'.
-class Translated r where
-  _translated :: r
-
-instance {-# OVERLAPPABLE #-} (TranslatedGo r t) => Translated r where
-  _translated = translatedGo id id
+instance {-# OVERLAPPABLE #-} (v ~ Double, TranslateGo r t) => TranslateGo (V3 v -> r) t where
+  translateGo keep acc v = translateGo keep (acc . W.translate v)
 
 class W.Transformable t => TranslatedGo r t | r -> t where
   translatedGo :: (t -> t) -> (t -> t) -> r
@@ -71,29 +94,43 @@ instance {-# OVERLAPPABLE #-} (v ~ V3, amt ~ Double, TranslatedGo r t) => Transl
         inverse = W.translate (0 & e .~ -amt)
      in translatedGo (forward . translation) (inverse . backward)
 
-class Translate2D a where
-  translate2D :: a
+class W.Transformable2D t => Translate2DGo r t | r -> t where
+  translate2DGo :: Bool -> (t -> t) -> r
 
-instance {-# INCOHERENT #-} (d ~ Double, e ~ Double, Transformable2D a, a' ~ a) => Translate2D (d -> e -> a -> a') where
-  translate2D x y a = W.translate2D (V2 x y) a
+-- base case
+instance {-# OVERLAPPABLE #-} (Num t, Transformable2D a, a' ~ a, a ~ t) => Translate2DGo (a -> a') t where
+  translate2DGo keep acc a = if keep then a + acc a else acc a
 
-instance {-# OVERLAPPABLE #-} (d ~ Double, Transformable2D a, a ~ a') => Translate2D (V2 d -> a -> a') where
-  translate2D v a = W.translate2D v a
+instance {-# INCOHERENT #-} (d ~ Double, e ~ Double, Translate2DGo r t) => Translate2DGo (d -> e -> r) t where
+  translate2DGo keep acc x y = translate2DGo keep (acc . W.translate2D (V2 x y))
 
--- | Linear defines 'ex' 'ey' 'ez'
---
--- > transform 'ex' 3 solid
-instance {-# OVERLAPPABLE #-} (v ~ V2, amt ~ Double, Transformable2D a, a' ~ a) => Translate2D (E v -> amt -> a -> a') where
-  translate2D (E e) amt a = W.translate2D (0 & e .~ amt) a
+instance {-# OVERLAPPABLE #-} (v ~ V2, amt ~ Double, Translate2DGo r t) => Translate2DGo (E v -> amt -> r) t where
+  translate2DGo keep acc (E e) amt = translate2DGo keep (acc . W.translate2D (0 & e .~ amt))
 
-class Translated2D a where
-  _translated2D :: a
+instance {-# OVERLAPPABLE #-} (v ~ Double, Translate2DGo r t) => Translate2DGo (V2 v -> r) t where
+  translate2DGo keep acc v = translate2DGo keep (acc . W.translate2D v)
 
-instance {-# INCOHERENT #-} (Profunctor p, Functor g, d ~ Double, e ~ Double, Transformable2D a, a' ~ a) => Translated2D (d -> e -> Optic' p g a a') where
-  _translated2D x y = iso (translate2D x y :: a' -> a) (translate2D (-x) (-y) :: a -> a')
+class W.Transformable2D t => Translated2DGo r t | r -> t where
+  translated2DGo :: (t -> t) -> (t -> t) -> r
 
-instance {-# OVERLAPPABLE #-} (Profunctor p, Functor g, d ~ Double, Transformable2D a, a ~ a') => Translated2D (V2 d -> Optic' p g a a') where
-  _translated2D v = iso (translate2D v :: a' -> a) (translate2D (-v) :: a -> a')
+-- base case
+instance {-# OVERLAPPABLE #-} (Transformable2D t, Profunctor p, Functor g, a' ~ a, a ~ t) => Translated2DGo (Optic' p g a a') t where
+  translated2DGo forward backward = iso (forward :: a' -> a) (backward :: a -> a')
 
-instance {-# OVERLAPPABLE #-} (Profunctor p, Functor g, v ~ V2, amt ~ Double, Transformable2D a, a' ~ a) => Translated2D (E v -> amt -> Optic' p g a a') where
-  _translated2D (E e) amt = iso (translate2D (0 & e .~ amt) :: a' -> a) (translate2D (0 & e .~ -amt) :: a -> a')
+instance {-# INCOHERENT #-} (d ~ Double, e ~ Double, Translated2DGo r t) => Translated2DGo (d -> e -> r) t where
+  translated2DGo forward backward x y =
+    let translation = W.translate2D (V2 x y)
+        inverse = W.translate2D (V2 (-x) (-y))
+     in translated2DGo (forward . translation) (inverse . backward)
+
+instance {-# OVERLAPPABLE #-} (d ~ Double, Translated2DGo r t) => Translated2DGo (V2 d -> r) t where
+  translated2DGo forward backward v =
+    let translation = W.translate2D v
+        inverse = W.translate2D (-v)
+     in translated2DGo (forward . translation) (inverse . backward)
+
+instance {-# OVERLAPPABLE #-} (v ~ V2, amt ~ Double, Translated2DGo r t) => Translated2DGo (E v -> amt -> r) t where
+  translated2DGo forward backward (E e) amt =
+    let translation = W.translate2D (0 & e .~ amt)
+        inverse = W.translate2D (0 & e .~ -amt)
+     in translated2DGo (forward . translation) (inverse . backward)
