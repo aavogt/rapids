687b745f82b359c3b329a3aef1609d9b341e06ea
Adam Vogt <vogt.adam@gmail.com>
2026-09-09 15:06:59 -0400

    offsetShape segfaults when the path is a single line segment

--- test/volume_offset.hs
+++ test/volume_offset.hs
@@ -3,3 +3,15 @@ import Solids
 
 main = do
   print [(x, volume (offset x bowtie)) | x <- [0.01, 0.1, 0.5, 1, 2]]
+  writeSTEP "volume_offset.step" ySolid
+
+
+ySolid :: Solid
+ySolid = fromPaths [ ln (V3 (w/2) (h/2) 0), ln (V3 (-w/2) (h/2) 0), ln (V3 0 (-h/2) 0) ]
+  where
+  ln to = line 0 to :: Path
+  h = 2
+  w = 1
+
+fromPaths xs = foldMap (pad t . toShape . offset t 1) xs
+  where t = 0.1
