516dde9b8f47e7dded9a538c6d0aeed423613a6a
Adam Vogt <vogt.adam@gmail.com>
2026-09-09 01:08:38 -0400

    work around https://github.com/joe-warren/opencascade-hs/issues/55

--- lib/Rapids/AABB/Lens.hs
+++ lib/Rapids/AABB/Lens.hs
@@ -6,6 +6,7 @@ import Rapids.Reexports
 import Rapids.Scale
 import Rapids.Translate
 import Data.Type.Equality (apply)
+import qualified Waterfall.Internal.NearZero as WNZ
 
 -- | edit the 'axisAlignedBoundingBox'
 aabb :: Lens' Solid (Maybe (V3 Double,V3 Double))
@@ -16,10 +17,15 @@ aabb f s =
 applyAABBTransform :: Solid -> Maybe (V3 Double, V3 Double) -> Maybe (V3 Double, V3 Double) -> Solid
 applyAABBTransform s (Just (a,b)) (Just (c,d)) = s
   & translate (-abCenter)
-  & scale ((d - c) / (b - a))
+  & scale (roundTo1 <$> (d - c) / (b - a))
   & translate cdCenter
   where
   abCenter = (a+b)/2
   cdCenter = (c+d)/2
 applyAABBTransform _ Nothing (Just cd) = aabbToSolid cd
 applyAABBTransform _ _ _ = mempty
+
+roundTo1 :: Double -> Double
+roundTo1 x
+  | WNZ.nearZero (x - 1) = 1
+  | otherwise = x
